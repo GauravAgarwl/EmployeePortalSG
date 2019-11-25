15af14bf98a364bfc7f21ee047c125a0d075c240
GauravAgarwl <gaurava337123@gmail.com>
2019-11-26 04:58:22 +0530

    Add files via upload

---
+++ EmployeePortalFrontEnd/src/app/_services/authentication.service.ts
@@ -0,0 +1,18 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: HttpClient) { }
+    
+    attemptAuth(username: string, password: string) {
+        const credentials = {username: username, password: password};
+        let headers = new HttpHeaders();
+        headers = headers.append('noToken', 'noToken');
+        console.log('attempAuth ::');
+        return this.http.post<any>('http://localhost:8080/token/generate-token', credentials, {headers: headers});
+      }
+
+    
+}
---
+++ EmployeePortalFrontEnd/src/app/_services/user.service.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../_models';
+import { Observable } from 'rxjs';
+import { TokenStorage } from '../_helpers';
+
+@Injectable()
+export class UserService {
+    private usersUrl ='http://localhost:8080/employeeDetails/register';
+    private useUrl1 ='http://localhost:8080/employeeDetails/getEmp';
+   
+    constructor(private http: HttpClient,private token: TokenStorage) { }
+  
+    public getAllEmployees(): Observable<User[]> {
+        return this.http.get<User[]>(this.useUrl1);
+      }
+    
+      public registerNewEmployee(user: User) {
+        let headers = new HttpHeaders();
+        headers = headers.append('noToken', 'noToken');
+        return this.http.post<User>(this.usersUrl, user,{headers: headers});
+    }
+
+}
