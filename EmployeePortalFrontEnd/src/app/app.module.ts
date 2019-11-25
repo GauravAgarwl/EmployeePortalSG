f3f961e4935b6eeb58691d20aabd3ddaa7bf31bd
GauravAgarwl <gaurava337123@gmail.com>
2019-11-26 05:02:13 +0530

    Add files via upload

---
+++ EmployeePortalFrontEnd/src/app/app.module.ts
@@ -0,0 +1,40 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+import { AlertComponent } from './_directives';
+import { AuthGuard } from './_guards';
+import { Interceptor,TokenStorage } from './_helpers';
+import { AlertService, AuthenticationService, UserService } from './_services';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';;
+import { RegisteredUsersComponent } from './registered-users/registered-users.component'
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        LoginComponent,
+        RegisterComponent,
+        RegisteredUsersComponent],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        TokenStorage,
+        { provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ EmployeePortalFrontEnd/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_guards';
+import { RegisteredUsersComponent } from './registered-users';
+
+const appRoutes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'viewAllUsers', component: RegisteredUsersComponent, canActivate: [AuthGuard] },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
