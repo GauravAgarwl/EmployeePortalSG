81ed98fc569dffc4369de9f26fde661448ada1f0
GauravAgarwl <gaurava337123@gmail.com>
2019-11-26 04:46:33 +0530

    Add files via upload

---
+++ EmployeePortalBackend/src/main/java/com/societe/generale/model/Constants.java
@@ -0,0 +1,9 @@
+package com.societe.generale.model;
+
+public class Constants {
+
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 3*60*60;
+    public static final String SIGNING_KEY = "gaurav123";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}
---
+++ EmployeePortalBackend/src/main/java/com/societe/generale/model/EmployeeModel.java
@@ -0,0 +1,87 @@
+package com.societe.generale.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_EmployeeModel")
+public class EmployeeModel {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+
+	private String firstName;
+		
+	private String lastName;
+
+	private String gender;
+
+	private Date dateOfBirth;
+	
+	private String department;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeModel [firstName=" + firstName + ", lastName=" + lastName + ", gender=" + gender
+				+ ", dateOfBirth=" + dateOfBirth + ", department=" + department + "]";
+	}
+
+	
+}
+
